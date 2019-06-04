08417a3ed9f57daeb8535ecfba58c97408bd44a5
Haley DeKeyser <hcd09@live.com>
2019-06-04 17:26:00 +0000

    welcome message

--- lib/pet_adoption/cli.rb
+++ lib/pet_adoption/cli.rb
@@ -1,5 +1,8 @@
 class PetAdoption::CLI
   def call
-    puts "Welcome to the Animal Humane Society of the Twin Cities!"
+    puts "Welcome to PAWS Chicago, a non-profit animal shelter organization!"
+    puts "Thank you for your interest in adopting a pet from one of our shelters!"
+    puts "What type of pet would you like to adopt?"
+    input = gets.strip
   end
 end
