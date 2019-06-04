f30160dc5d7ed85208e870cc2df4a472214dfded
Haley DeKeyser <hcd09@live.com>
2019-06-04 19:37:08 +0000

    scrape_index_page

--- lib/pet_adoption/cli.rb
+++ lib/pet_adoption/cli.rb
@@ -1,14 +1,14 @@
 class PetAdoption::CLI
   def call
-    puts "Welcome to PAWS Chicago, a non-profit animal shelter organization!"
+    puts "Welcome to PAWS Chicago!"
     puts "Thank you for your interest in adopting a pet from one of our shelters!"
     menu
   end
   
   def menu
     puts "Our shelters have cats and dogs available for adoption."
-    puts "To view all of the available cats, please enter cat."
-    puts "To view all of the available dogs, please enter dog."
+    puts "To view a list of the available cats, please enter cat."
+    puts "To view a list of the available dogs, please enter dog."
     puts "Otherwise, enter exit."
     input = nil
     while input != "exit"
@@ -23,11 +23,11 @@ class PetAdoption::CLI
     end
   end
   
-  def get_cats
+  def list_cats
     #lists all of the cats available for adoption
   end
   
-  def get_dogs
+  def list_dogs
     #lists all of the dogs available for adoption
   end
   
--- lib/pet_adoption/scraper.rb
+++ lib/pet_adoption/scraper.rb
@@ -1,6 +1,29 @@
 require 'nokogiri'
 require 'open-uri'
+require 'pry'
 
 class Scraper
+  BASE_PATH = htpps://www.pawschicago.org/
   
+  def self.scrape_index_page(BASE_PATH)
+    index_page = Nokogiri::HTML(open(BASE_PATH + "our-work/pet-adoption/pets-available/"))
+    pets = []
+    pet_classes = ["cats", "dogs"]
+    pet_classes.each do |class|
+      index_page.css("div.pets-placeholder article.#{class}").each do |pet_block|
+        pet.css("div.adopt-pet a").each do |pet|
+          pet_profile_link = "#{pet.attr("href")}"
+          pet_name = pet.css("h3").text
+          pet_location = pet.css("h6").text
+          pet_type = "#{class}"
+          pets << { name: pet_name, location: pet_location, type: pet_type, profile_link: pet_profile_link }
+          binding.pry
+        end
+      end
+    end
+
+        
+    
+    pets
+  end
 end
