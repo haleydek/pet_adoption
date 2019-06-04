5f85a1af761d2a4f7bce5f9ec044b29eab37e4fa
Haley DeKeyser <hcd09@live.com>
2019-06-04 15:42:54 +0000

    first commit

---
+++ lib/pet_adoption.rb
@@ -0,0 +1,6 @@
+require "pet_adoption/version"
+
+module PetAdoption
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/pet_adoption/version.rb
@@ -0,0 +1,3 @@
+module PetAdoption
+  VERSION = "0.1.0"
+end
