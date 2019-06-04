164e59de88892055f2a2bdc192b06bd1a5e718a4
Haley DeKeyser <hcd09@live.com>
2019-06-04 17:54:38 +0000

    menu

--- lib/pet_adoption.rb
+++ lib/pet_adoption.rb
@@ -1,2 +1,3 @@
 require_relative "./pet_adoption/version"
+require_relative "./pet_adoption/scraper"
 require_relative "./pet_adoption/cli"
--- lib/pet_adoption/cli.rb
+++ lib/pet_adoption/cli.rb
@@ -2,7 +2,33 @@ class PetAdoption::CLI
   def call
     puts "Welcome to PAWS Chicago, a non-profit animal shelter organization!"
     puts "Thank you for your interest in adopting a pet from one of our shelters!"
-    puts "What type of pet would you like to adopt?"
-    input = gets.strip
+    menu
   end
+  
+  def menu
+    puts "Our shelters have cats and dogs available for adoption."
+    puts "To view all of the available cats, please enter cat."
+    puts "To view all of the available dogs, please enter dog."
+    puts "Otherwise, enter exit."
+    input = nil
+    while input != "exit"
+      case input
+        when "cat"
+          puts "list of cats"
+          #execute list of cats
+        when "dog"
+          puts "list of dogs"
+          #execute list of dogs
+      end
+    end
+  end
+  
+  def get_cats
+    #lists all of the cats available for adoption
+  end
+  
+  def get_dogs
+    #lists all of the dogs available for adoption
+  end
+  
 end
