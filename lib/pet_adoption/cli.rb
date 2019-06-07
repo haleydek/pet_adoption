617e165636e31183915135e90f17ef3e170b1d7d
Haley DeKeyser <hcd09@live.com>
2019-06-07 18:45:48 +0000

    print pet attributes

--- lib/pet_adoption/cli.rb
+++ lib/pet_adoption/cli.rb
@@ -8,11 +8,12 @@ class PetAdoption::CLI
     PetAdoption::Scraper.create_pets
     PetAdoption::Scraper.scrape_pet_attributes
     puts "Welcome to the Twin Cities' Animal Humane Society!"
-    puts "Thank you for your interest in adopting a pet from one of our shelters!\n"
+    puts "Thank you for your interest in adopting a pet from one of our shelters!"
     menu
   end
   
   def menu
+    
     puts "\nOur shelters have the following types of animals available for adoption: "
 
     print_species
@@ -20,9 +21,17 @@ class PetAdoption::CLI
     puts "\nWhich type of animal are you interested in adopting? Please enter the corresponding number to view the available pets."
     puts "Otherwise, enter exit."
     
-    input = gets.strip
+    input_1 = gets.strip
     
-    print_pets_from_species(input)
+    species = get_species_from_name(input_1)
+    
+    print_pets_from_species(species)
+    
+    input_2 = gets.strip
+    
+    pet = get_pet(species, input_2)
+    
+    print_pet_attributes(pet)
     
     #scrape species page to get list of pets
     #print list of pets
@@ -51,15 +60,22 @@ class PetAdoption::CLI
     PetAdoption::Species.all.each_with_index { |species, i| puts "#{i + 1}. #{species.name}" }
   end
   
-  def get_species_from_name(input)
-    PetAdoption::Species.all[input.to_i - 1]
+  def get_species_from_name(input_1)
+    PetAdoption::Species.all[input_1.to_i - 1]
   end
   
-  def print_pets_from_species(input)
-    species = get_species_from_name(input)
+  def print_pets_from_species(species)
     species.pets.each_with_index do |pet, index|
       puts "#{index + 1}. #{pet.name} - #{pet.breed}"
     end
+  end
+  
+  def get_pet(species, input_2)
+    species.pets[input_2.to_i - 1]
+  end
+  
+  def print_pet_attributes(pet)
+    pet.each { |key, value| puts "\n#{key}: #{value}" }
   end
   
 end
